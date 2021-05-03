7993d00c2c0986b84cbda6e4b3738f16cf3016ca
Xiande Qiu <Xiandeqiu@gmail.com>
2021-05-03 03:07:02 +0200

    .

--- public/script.js
+++ public/script.js
@@ -1,18 +1,22 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
+
+//PeerJS provides a complete, configurable, and easy-to-use peer-to-peer API built on top of WebRTC, supporting both data channels and media streams.
 const myPeer = new Peer(undefined, {
   host: '/',
   port: '3001'
 })
 const myVideo = document.createElement('video')
+//mute our microphone
 myVideo.muted = true
 const peers = {}
 
-//get permission
+//get permission for audio and video
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
+  //start streaming
   addVideoStream(myVideo, stream)
 
   myPeer.on('call', call => {
@@ -23,16 +27,19 @@ navigator.mediaDevices.getUserMedia({
     })
   })
 
+  //let other user connect
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
   })
 })
 
+//disconnect
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close()
 })
 
 myPeer.on('open', id => {
+  //myPeer will generate a random User id
   socket.emit('join-room', ROOM_ID, id)
 })
 
@@ -45,14 +52,16 @@ function connectToNewUser(userId, stream) {
   call.on('close', () => {
     video.remove()
   })
-
   peers[userId] = call
 }
 
 function addVideoStream(video, stream) {
+  //set the source
   video.srcObject = stream
+  //add an event to video
   video.addEventListener('loadedmetadata', () => {
     video.play()
   })
+  //will add the video to videoGrid
   videoGrid.append(video)
 }
--- server.js
+++ server.js
@@ -1,29 +1,39 @@
+const { createPublicKey } = require('crypto')
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-//get random id
+
+//get a random id
 const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 
+
 app.get('/', (req, res) => {
+  // redirect to dynamic room
   res.redirect(`/${uuidV4()}`)
 })
 
-//going
+
 app.get('/:room', (req, res) => {
+  //go to room view
   res.render('room', { roomId: req.params.room })
 })
 
+//when we connect with socket we will join the room
 io.on('connection', socket => {
+  //pass roomId and userId when we join a room
   socket.on('join-room', (roomId, userId) => {
+    //join the room
     socket.join(roomId)
+    //Send a message to everyone else in the room (excluding us)
     socket.to(roomId).broadcast.emit('user-connected', userId)
 
     socket.on('disconnect', () => {
+      //if we disconnect we will send a message to the room saying e are connecting
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
   })
